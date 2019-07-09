e10da09c7edf80745bcb3e16a6ee84d252aa4b4e
James Butterworth <jjbsports16@hotmail.co.uk>
2019-07-09 17:15:00 +0100

    Sends images as video

--- sensor_client/main_client.py
+++ sensor_client/main_client.py
@@ -4,49 +4,84 @@ import picamera
 import socket
 import struct
 
-if __name__ == "__main__":
+def establish_connection(host, port):
 
-    # Connect client socket to server
-    host = '192.168.1.69'
-    port = 8000
-
+    # Create client socket to the server
     client_socket = socket.socket()
     client_socket.connect((host, port))
 
     # Create file-like object out of the connection
     connection = client_socket.makefile('wb')
 
+    return client_socket, connection
+
+def send_images(connection):
+
+    with picamera.PiCamera() as camera:
+
+        camera.resolution = (640, 480)
+        camera.start_preview()
+        time.sleep(2)
+
+        start = time.time()
+        stream = io.BytesIO()
+
+        for _ in camera.capture_continuous(stream, 'jpeg'):
+
+            # Write length of the capture to stream and flush
+            connection.write(struct.pack('<L', stream.tell()))
+            connection.flush()
+
+            # Rewind the stream and send the image data
+            stream.seek(0)
+            connection.write(stream.read())
+
+            # Quit if we have beebn capturing for more than 30s
+            if time.time() - start > 30:
+                break;
+
+            # Reset the stream for next capture
+            stream.seek(0)
+            stream.truncate()
+
+    # Write a length of zero to stream to signal we are done
+    connection.write(struct.pack('<L', 0))
+
+def send_video(connection):
+
+    with picamera.PiCamera() as camera:
+
+        camera.resolution = (640, 480)
+        camera.framerate = 24
+
+        camera.start_preview()
+        time.sleep(2)
+
+        # Start recording, sending the output to the connection for 60
+        # seconds then stop
+        camera.start_recording(connection, format='h264')
+        camera.wait_recording(30)
+        camera.stop_recording()
+
+if __name__ == "__main__":
+
+    # Either send as video or images
+    send_as_video = True
+
+    # Connect client socket to server
+    host = '192.168.1.69'
+    port = 8000
+
+    client_socket, connection = establish_connection(host, port)
+
     try:
-        with picamera.PiCamera() as camera:
 
-            camera.resolution = (640, 480)
-            camera.start_preview()
-            time.sleep(2)
-
-            start = time.time()
-            stream = io.BytesIO()
-
-            for _ in camera.capture_continuous(stream, 'jpeg'):
-
-                # Write length of the capture to stream and flush
-                connection.write(struct.pack('<L', stream.tell()))
-                connection.flush()
-
-                # Rewind the stream and send the image data
-                stream.seek(0)
-                connection.write(stream.read())
-
-                # Quit if we have beebn capturing for more than 30s
-                if time.time() - start > 30:
-                    break;
-
-                # Reset the stream for next capture
-                stream.seek(0)
-                stream.truncate()
-
-        # Write a length of zero to stream to signal we are done
-        connection.write(struct.pack('<L', 0))
+        if send_as_video:
+            send_video(connection)
+        else:
+            send_images(connection)
 
     finally:
+
         connection.close()
         client_socket.close()
--- sensor_server/main_server.py
+++ sensor_server/main_server.py
@@ -4,12 +4,11 @@ import io
 #from PIL import Image
 import numpy as np
 import cv2
+import subprocess
 
-if __name__ == "__main__":
+def bind_socket(port):
 
     # Start listening to socket
-    port = 8000
-
     server_socket = socket.socket()
     server_socket.bind(('0.0.0.0', port))
     server_socket.listen(0)
@@ -18,36 +17,77 @@ if __name__ == "__main__":
     # This waits for data being sent to the socket before executing
     connection = server_socket.accept()[0].makefile('rb')
 
+    return server_socket, connection
+
+def read_images(connection):
+
+    while True:
+
+        # Read the length of the image as a 32-bit unsigned int. If the
+        # length is zero, quit the loop
+        image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
+        if not image_len:
+            break
+
+        # Construct a stream to hold the image data and read the image
+        # data from the connection
+        image_stream = io.BytesIO()
+        image_stream.write(connection.read(image_len))
+
+        # Rewind the stream
+        image_stream.seek(0)
+
+        # Open image with PIL and do some processing on it
+        #image = Image.open(image_stream)
+        #image.show()
+        #print('Image is %dx%d' % image.size)
+        #image.verify()
+        #print('Image is verified')
+
+        # Open image with OpenCV
+        file_bytes = np.asarray(bytearray(image_stream.read()), dtype=np.uint8)
+        img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+        cv2.imshow('Image', img)
+        cv2.waitKey(1)
+
+def read_video(connection):
+
+    vlc_command = '/Applications/VLC.app/Contents/MacOS/VLC'
+
+    # Run a viewer with an appropriate command line
+    cmdline = [vlc_command, '--demux', 'h264', '-']
+    player = subprocess.Popen(cmdline, stdin=subprocess.PIPE)
+
     try:
 
         while True:
 
-            # Read the length of the image as a 32-bit unsigned int. If the
-            # length is zero, quit the loop
-            image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
-            if not image_len:
+            # Repeatedly read 1k of data from the content and write it to
+            # the media player's stdin
+            data = connection.read(1024)
+            if not data:
                 break
+            player.stdin.write(data)
 
-            # Construct a stream to hold the image data and read the image
-            # data from the connection
-            image_stream = io.BytesIO()
-            image_stream.write(connection.read(image_len))
+    finally:
 
-            # Rewind the stream
-            image_stream.seek(0)
+        player.terminate()
 
-            # Open image with PIL and do some processing on it
-            #image = Image.open(image_stream)
-            #image.show()
-            #print('Image is %dx%d' % image.size)
-            #image.verify()
-            #print('Image is verified')
+if __name__ == "__main__":
 
-            # Open image with OpenCV
-            file_bytes = np.asarray(bytearray(image_stream.read()), dtype=np.uint8)
-            img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
-            cv2.imshow('Image', img)
-            cv2.waitKey(1)
+    # Either receive as images or video
+    receive_as_video = True
+
+    port = 8000
+
+    server_socket, connection = bind_socket(port)
+
+    try:
+
+        if receive_as_video:
+            read_video(connection)
+        else:
+            read_images(connection)
 
     finally:
 
