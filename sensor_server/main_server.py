fea76814f8ed3c2138b803cc6e91983c9bc8e883
James Butterworth <jjbsports16@hotmail.co.uk>
2019-07-09 16:13:52 +0100

    Open images server side with OpenCV

--- sensor_server/main_server.py
+++ sensor_server/main_server.py
@@ -1,7 +1,9 @@
 import socket
 import struct
 import io
-from PIL import Image
+#from PIL import Image
+import numpy as np
+import cv2
 
 if __name__ == "__main__":
 
@@ -25,21 +27,32 @@ if __name__ == "__main__":
             image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
             if not image_len:
                 break
+
             # Construct a stream to hold the image data and read the image
             # data from the connection
             image_stream = io.BytesIO()
             image_stream.write(connection.read(image_len))
-            # Rewind the stream, open it as an image with PIL and do some
-            # processing on it
+
+            # Rewind the stream
             image_stream.seek(0)
-            image = Image.open(image_stream)
-            image.show()
-            print('Image is %dx%d' % image.size)
-            image.verify()
-            print('Image is verified')
+
+            # Open image with PIL and do some processing on it
+            #image = Image.open(image_stream)
+            #image.show()
+            #print('Image is %dx%d' % image.size)
+            #image.verify()
+            #print('Image is verified')
+
+            # Open image with OpenCV
+            file_bytes = np.asarray(bytearray(image_stream.read()), dtype=np.uint8)
+            img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+            cv2.imshow('Image', img)
+            cv2.waitKey(1)
 
     finally:
+
         connection.close()
         server_socket.close()
+        print("Sockets closed")
 
     print("Finished")
