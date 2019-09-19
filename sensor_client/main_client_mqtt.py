04fd1eec87e0d46ded99972d2e64bb7efffd31d2
James <jjbsports16@hotmail.co.uk>
2019-09-19 16:35:09 +0100

    Cleaning up RE code

--- sensor_client/main_client_mqtt.py
+++ sensor_client/main_client_mqtt.py
@@ -8,14 +8,20 @@ from time import sleep
 
 if __name__ == "__main__":
 
-    james_mac_addr = '192.168.1.69'
-    james_linux_home_addr = '192.168.1.66'
     sensor_city_addr = 'sensorcity.io'
     mqtt_broker_addr = sensor_city_addr
     mqtt_port = 1883
 
     res_height = 120
     res_width = 160
+
+    growth_clk_pin = 17
+    growth_dt_pin = 18
+    growth_sw_pin = 27
+
+    decay_clk_pin = 22
+    decay_dt_pin = 23
+    decay_sw_pin = 24
 
     # Connect to MQTT broker
     client = mqtt.Client()
@@ -24,11 +30,19 @@ if __name__ == "__main__":
     client.connect(mqtt_broker_addr, mqtt_port, 60)
     print("..connected")
 
-    # Start listening to Rotary Encoder
-    rotary_encoder = RotaryEncoder(client)
-    rotary_listener = Process(target=rotary_encoder.listen)
-    rotary_listener.start()
+    # Start listening to both Rotary Encoders
+    growth_rotary_encoder = RotaryEncoder(client, "growth-rate", growth_clk_pin, growth_dt_pin, growth_sw_pin)
+    growth_rotary_listener = Process(target=growth_rotary_encoder.listen)
+    growth_rotary_listener.start()
 
+    decay_rotary_encoder = RotaryEncoder(client, "decay-rate", decay_clk_pin, decay_dt_pin, decay_sw_pin)
+    decay_rotary_listener = Process(target=decay_rotary_encoder.listen)
+    decay_rotary_listener.start()
+
+    while True:
+        continue
+
+    '''
     # Set up pi camera
     with picamera.PiCamera() as camera:
 
@@ -46,10 +60,12 @@ if __name__ == "__main__":
             client.publish("heat-map/rgb-stream", stream.read())
 
             # Quit if we have been capturing for more than 30s
-            if time.time() - start > 30:
+            if time.time() - start > 120:
                 break
 
             stream.seek(0)
             stream.truncate()
+    '''
 
-    rotary_listener.join()
+    growth_rotary_listener.join()
+    decay_rotary_listener.join()
--- sensor_client/rotary_encoder.py
+++ sensor_client/rotary_encoder.py
@@ -3,29 +3,43 @@ from time import sleep
 
 class RotaryEncoder:
 
-    def __init__(self, mqtt_client):
+    def __init__(self, mqtt_client, mqtt_topic_name, clk_pin, dt_pin, sw_pin):
 
         self.mqtt_client = mqtt_client
+        self.mqtt_topic_name = "heat-map/rotary-encoder/" + mqtt_topic_name
 
+        self.clk = clk_pin
+        self.dt = dt_pin
+        self.sw = sw_pin
+
+        '''
         self.clk_growth = 17
         self.dt_growth = 18
+        self.sw_growth = 27
 
         self.clk_decay = 22
         self.dt_decay = 23
+        self.sw_decay = 24
+        '''
 
         GPIO.setmode(GPIO.BCM)
 
-        GPIO.setup(self.clk_growth, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.setup(self.dt_growth, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
+        GPIO.setup(self.clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(self.dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(self.sw, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        '''
         GPIO.setup(self.clk_decay, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.setup(self.dt_decay, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(self.sw_decay, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
         self.growth_counter = 0
         self.decay_counter = 0
         self.clk_growth_last_state = GPIO.input(self.clk_growth)
         self.clk_decay_last_state = GPIO.input(self.clk_decay)
+        '''
 
+        self.counter = 0
+        self.clk_last_state = GPIO.input(self.clk)
 
     def listen(self):
 
@@ -33,8 +47,10 @@ class RotaryEncoder:
 
             while True:
 
+                '''
                 clk_growth_state = GPIO.input(self.clk_growth)
                 dt_growth_state = GPIO.input(self.dt_growth)
+                sw_growth_state = GPIO.input(self.sw_growth)
 
                 if clk_growth_state != self.clk_growth_last_state:
                     if dt_growth_state != clk_growth_state:
@@ -43,10 +59,10 @@ class RotaryEncoder:
                     else:
                         self.growth_counter -= 1
                         self.mqtt_client.publish("heat-map/rotary-encoder/growth_rate", 0)
-                    print("Growth counter:", self.growth_counter)
 
                 clk_decay_state = GPIO.input(self.clk_decay)
                 dt_decay_state = GPIO.input(self.dt_decay)
+                sw_decay_state = GPIO.input(self.sw_decay)
 
                 if clk_decay_state != self.clk_decay_last_state:
                     if dt_decay_state != clk_decay_state:
@@ -55,10 +71,32 @@ class RotaryEncoder:
                     else:
                         self.decay_counter -= 1
                         self.mqtt_client.publish("heat-map/rotary-encoder/decay_rate", 0)
-                    print("Decay counter:", self.decay_counter)
+
+                if sw_growth_state == 0 or sw_decay_state == 0:
+                    self.mqtt_client.publish("heat-map/rotary-encoder/switch", 1)
 
                 self.clk_growth_last_state = clk_growth_state
                 self.clk_decay_last_state = clk_decay_state
+                sleep(0.01)
+                '''
+
+                clk_state = GPIO.input(self.clk)
+                dt_state = GPIO.input(self.dt)
+                sw_state = GPIO.input(self.sw)
+
+                if clk_state != self.clk_last_state:
+                    if dt_state != clk_state:
+                        self.counter += 1
+                        self.mqtt_client.publish(self.mqtt_topic_name, 1)
+                    else:
+                        self.counter -= 1
+                        self.mqtt_client.publish(self.mqtt_topic_name, 0)
+                    print(self.counter)
+
+                if sw_state == 0:
+                    self.mqtt_client.publish("heat-map/rotary-encoder/switch", 1)
+
+                self.clk_last_state = clk_state
                 sleep(0.01)
 
         finally:
